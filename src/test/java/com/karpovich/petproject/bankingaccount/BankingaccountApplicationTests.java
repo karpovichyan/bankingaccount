ebd524a3f1048ee776376e47654cf118564cbc71
karpovichyan <karpovichjan@gmail.com>
2020-12-17 21:37:12 +0300

    Initial commit

---
+++ src/test/java/com/karpovich/petproject/bankingaccount/BankingaccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karpovich.petproject.bankingaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingaccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
